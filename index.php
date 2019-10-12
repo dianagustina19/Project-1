dabadb6ede812839bda3d66537d6efe263d5d563
dianagustina19 <56467164+dianagustina19@users.noreply.github.com>
2019-10-12 18:58:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Dian Agustina Portofolio</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: BizPage
+    Theme URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">Dian Agustina</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#portfolio">Project</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/bg.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <b><h2>Web Developer</h2></b>
+                <b><h5>Try Hard, Hard Work, Success</h5></b>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>About Me</h3>
+          <div class="row">
+            <div class="col-4">
+              <br><br><br> <br><br><br> <br><br><br>
+              <p>Fresh Graduate with a bachelor's degree in information sysrem from Gunadarama University. I am a person who has passion and good discipline that is always applied. Good at analyst, planning and communication, I am very passionate in doing website develpoment, and i have experience creating website.</p>
+            </div>
+            <div class="col-8">
+              <center><img src="img/mee.jpg" width="500" height="800"></center><br>
+            </div>
+        </header><br><br>
+        
+      <section id="skill"  class="section-bg" >
+          <div class="container">
+         <header class="section-header">
+          <h3 class="section-title">Skill</h3>
+        </header>
+        <div class="row about-cols">
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/frontend.png" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-code"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Backend</a></h2>
+            <p>
+               <ul>
+               <b>
+                 <li>PHP</li>
+                 <li>Laravel</li>
+                 <li>MySQL</li>
+               </b>
+             </ul>
+            </p>
+             <br>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/frontend2.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-laptop"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Frontend</a></h2>
+            <p>
+               <ul>
+               <b>
+                 <li>HTML 5</li>
+                 <li>CSS</li>
+                 <li>Bootstrap</li>
+                 <li>JavaScipt</li>
+                 <li>JQuery</li>
+               </b>
+             </ul>
+            </p>
+             <br>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/cc.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-database"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Other</a></h2>
+              <p>
+                <ul>
+                  <b>
+                    <li>Microsoft Office</li>
+                    <li>C Programming Language</li>
+                    <li>Bahasa</li>
+                    <li>English</li>
+                  </b>
+                </ul>
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    
+
+    <!--==========================
+      Portfolio Section
+    ============================-->
+    <section id="portfolio"  class="section-bg" >
+      <div class="container">
+
+        <header class="section-header">
+          <h3 class="section-title">Project</h3>
+        </header>
+          <div class="row">
+            <div class="col-8">
+              <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-interval="1000">
+                    <img src="img/P3/1.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P3/2.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P3/3.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P3/4.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                </div>
+                 <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+              </div>
+            </div>
+            <div class="col-4">
+              <h2><b>SISPEN</b></h2>
+              <div style="text-align: justify;">
+                <p>This was my thesis,It's a Website that manages the recording of public health service activities such as posyandu for toddlers, posbindu for adults and posbindu for elderly people.</p>
+              </div>
+              <h5><b>What I used :</b></h5>
+              <ul>
+                <li>HTML 5</li>
+                <li>PHP</li>
+                <li>JavaScript</li>
+                <li>MySQL</li>
+                <li>Bootstrap</li>
+              </ul>
+              <a href="http://kesehatanindonesia.000webhostapp.com" class="btn btn-outline-primary"><i class="fa fa-eye"></i> Visit Site</a>
+            </div>
+          </div><br><br><br><br><br><br>
+
+          <div class="row">
+            <div class="col-4">
+              <h2><b>E-ART</b></h2>
+              <p>E-Commerce Websites that are used to sell handicraft items.</p>
+              <h5><b>What I used :</b></h5>
+              <ul>
+                <li>HTML 5</li>
+                <li>PHP</li>
+                <li>JavaScript</li>
+                <li>MySQL</li>
+                <li>Bootstrap</li>
+              </ul>
+               <a href="" class="btn btn-outline-primary"><i class="fa fa-eye"></i> Visit Site</a>
+            </div>
+            <div class="col-8">
+              <div id="carouselExampleInterval2" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-interval="1000">
+                    <img src="img/P2/1.png" class="d-block w-100" alt="..." wwidth="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P2/2.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P2/3.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P2/4.png" class="d-block w-100" alt="..." wwidth="700" height="500">
+                  </div>
+                </div>
+                 <a class="carousel-control-prev" href="#carouselExampleInterval2" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleInterval2" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+              </div>
+            </div>
+          </div><br><br><br><br><br><br>
+          
+
+          <div class="row">
+            <div class="col-8">
+              <div id="carouselExampleInterval3" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-interval="1000">
+                    <img src="img/P1/1.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P1/2.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P1/3.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                  <div class="carousel-item" data-interval="1000">
+                    <img src="img/P1/4.png" class="d-block w-100" alt="..." width="700" height="500">
+                  </div>
+                </div>
+                 <a class="carousel-control-prev" href="#carouselExampleInterval3" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleInterval3" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+              </div>
+            </div>
+            <div class="col-4">
+              <h2><b>Maju Jaya Teknik</b></h2>
+              <div style="text-align: justify;">
+                <p>this is my previous scientific writing. E-commerce website Maju Jaya Teknik is a business engaged in the service or air conditioner. The system used in this shop is still manual in terms of promoting new products so I changed the manual system to an online system. then this website can besides promoting new products, offering services and products that are directly sold on this website.</p>
+              </div>
+              <h5><b>What I used :</b></h5>
+              <ul>
+                <li>HTML 5</li>
+                <li>PHP</li>
+                <li>JavaScript</li>
+                <li>MySQL</li>
+                <li>Bootstrap</li>
+              </ul>
+               <a href="" class="btn btn-outline-primary"><i class="fa fa-eye"></i> Visit Site</a>
+            </div>
+          </div><br><br><br>
+
+
+        </div>
+        </div>
+
+
+    </section><!-- #portfolio -->
+
+
+
+
+    <!--==========================
+      Contact Section
+    ============================-->
+    <section id="contact" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h3>Contact</h3>
+          
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="fa fa-envelope"></i>
+              <h3>Email</h3>
+              <p><a href="dian.agustina206@gmail.com">dian.agustina206@gmail.com</a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="fa fa-linkedin"></i><br>
+              <h3>Linkedln</h3>
+              <p><a href="https://www.linkedin.com/in/dian-agustina-b07b9a156/">Dian Agustina</a></p>
+            </div>
+          </div>
+
+          
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="fa fa-github"></i>
+              <h3>Github</h3>
+              <p><a href="https://github.com/dianagustina19">dianagustina19</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Dian Agustina</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+        -->
+        <!--Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>-->
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
